8c534c0214946d7563882ed6067b1f8393afc4ab
hamaiook <dososokung@gmail.com>
2014-03-14 11:16:23 +0700

    Fizzbuzz refactor

--- fizzbuzz/FizzbuzzTest.php
+++ fizzbuzz/FizzbuzzTest.php
@@ -15,40 +15,39 @@ class FizzbuzzTest extends PHPUnit_Framework_TestCase {
   function testInput1ShouldReturn1() {
     $expected = 1;
     $input = 1;
-    $fizzbuzz = new Fizzbuzz();
-    $result = $fizzbuzz->validateFizzbuzz($input);
+    $result = $this->countFizzbuzz($input);
     $this->assertEquals($expected, $result);
   }
 
   function testInput2ShouldReturn2() {
     $expected = 2;
     $input = 2;
-    $fizzbuzz = new Fizzbuzz();
-    $result = $fizzbuzz->validateFizzbuzz($input);
+    $result = $this->countFizzbuzz($input);
     $this->assertEquals($expected, $result);
   }
   
   function testInput3ShouldReturnFizz() {
     $expected = 'Fizz';
     $input = 3;
-    $fizzbuzz = new Fizzbuzz();
-    $result = $fizzbuzz->validateFizzbuzz($input);
+    $result = $this->countFizzbuzz($input);
     $this->assertEquals($expected, $result);
   }
   
   function testInput4ShouldReturn4() {
     $expected = 4;
     $input = 4;
-    $fizzbuzz = new Fizzbuzz();
-    $result = $fizzbuzz->validateFizzbuzz($input);
+    $result = $this->countFizzbuzz($input);
     $this->assertEquals($expected, $result);
   }
 
   function testInput5ShouldReturnBuzz(){
     $expected = 'Buzz';
     $input = 5;
-    $fizzbuzz = new Fizzbuzz();
-    $result = $fizzbuzz->validateFizzbuzz($input);
+    $result = $this->countFizzbuzz($input);
     $this->assertEquals($expected, $result);
+  }
+  function countFizzbuzz($input) {
+    $fizzbuzz = new Fizzbuzz();
+    return $fizzbuzz->validateFizzbuzz($input);
   }
 }

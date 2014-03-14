66fff57b07fd304b18bb90679b3bd0227630ee16
hamaiook <dososokung@gmail.com>
2014-03-14 11:56:15 +0700

    Replace dataprovider to test cate 10

--- fizzbuzz/FizzbuzzTest.php
+++ fizzbuzz/FizzbuzzTest.php
@@ -4,7 +4,7 @@ class Fizzbuzz {
     if(($input%3)==0){
       return 'Fizz';
     }
-    if($input==5){
+    if(($input%5)==0){
       return 'Buzz';
     }
     return $input;
@@ -18,7 +18,8 @@ class FizzbuzzTest extends PHPUnit_Framework_TestCase {
       array(3,"Fizz"),
       array(4,4),
       array(5,"Buzz"),
-      array(6,"Fizz"));
+      array(6,"Fizz"),
+      array(10,"Buzz"));
   }
 
   /**
